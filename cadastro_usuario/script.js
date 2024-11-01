2a96e4e069f8c65dcaefc1e4ec196bfcba2a8628
dogo-o <gabriel.luanh@yahoo.com.br>
2024-10-31 23:10:26 -0300

    Update script.js

--- cadastro_usuario/script.js
+++ cadastro_usuario/script.js
@@ -1,12 +1,12 @@
 const botoes = document.querySelectorAll('#btn_usuario, #btn_nutri, #btn_treinador');
 
-function aplicarEstiloAtivo(botaoAtivo) {
+function aplicarEstiloAtivo(event) {
     for (botao of botoes) {
         botao.classList.remove('ativo');
     }
-    botaoAtivo.target.classList.add('ativo');
+    event.target.classList.add('ativo');
 }
 
-for (botao of botoes) {
-    botao.addEventListener('click', aplicarEstiloAtivo);
-}
+btn_usuario.addEventListener('click', aplicarEstiloAtivo);
+btn_nutri.addEventListener('click', aplicarEstiloAtivo);
+btn_treinador.addEventListener('click', aplicarEstiloAtivo);
