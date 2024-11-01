b7ba06df849ec274d4da0793b224dd20f61a3450
dogo-o <gabriel.luanh@yahoo.com.br>
2024-11-01 02:08:51 -0300

    formularios adicionados

--- cadastro_usuario/script.js
+++ cadastro_usuario/script.js
@@ -1,12 +1,33 @@
 const botoes = document.querySelectorAll('#btn_usuario, #btn_nutri, #btn_treinador');
+const forms = document.querySelectorAll('#formulario_usuario, #formulario_nutricionista, #formulario_treinador');
 
-function aplicarEstiloAtivo(event) {
-    for (botao of botoes) {
-        botao.classList.remove('ativo');
+function aplicarEstiloAtivo(botao) {
+    const idBotao = document.getElementById(botao);
+    for (btn of botoes) {
+        btn.classList.remove('ativo');
     }
-    event.target.classList.add('ativo');
+    idBotao.classList.add('ativo');
 }
 
-btn_usuario.addEventListener('click', aplicarEstiloAtivo);
-btn_nutri.addEventListener('click', aplicarEstiloAtivo);
-btn_treinador.addEventListener('click', aplicarEstiloAtivo);
+function mostrarFormulario(formulario) {
+    const idForm = document.getElementById(formulario);
+    for (formularios of forms) {
+        formularios.style.display = 'none';
+    }
+    idForm.style.display = 'block';
+}
+
+document.getElementById('btn_usuario').addEventListener('click', function() {
+    aplicarEstiloAtivo('btn_usuario');
+    mostrarFormulario('formulario_usuario');
+});
+document.getElementById('btn_nutri').addEventListener('click', function() {
+    aplicarEstiloAtivo('btn_nutri');
+    mostrarFormulario('formulario_nutricionista');
+});
+document.getElementById('btn_treinador').addEventListener('click', function() {
+    aplicarEstiloAtivo('btn_treinador');
+    mostrarFormulario('formulario_treinador');
+});
+
+
