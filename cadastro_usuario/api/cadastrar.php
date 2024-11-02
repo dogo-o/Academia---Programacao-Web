48840634b46c9ed7b587477e487a1490f2b04548
dogo-o <gabriel.luanh@yahoo.com.br>
2024-11-02 02:19:13 -0300

    Update cadastrar.php

--- cadastro_usuario/api/cadastrar.php
+++ cadastro_usuario/api/cadastrar.php
@@ -1,5 +1,6 @@
 <?php 
-include_once('config.php');
+include 'config.php';
+
 $nome_usuario = $_POST['nome_usuario'];
 $email_usuario = $_POST['email_usuario'];
 $cpf_usuario = $_POST['cpf_usuario'];
@@ -8,18 +9,36 @@ $genero_usuario = $_POST['genero_usuario'];
 $senha_usuario = $_POST['senha_usuario'];
 $senha_usuario_hash = password_hash($senha_usuario, PASSWORD_DEFAULT);
 
-$sql = 'INSERT INTO usuarios (nome_usuario, email_usuario, CPF_usuario, data_nascimento_usuario, genero_usuario, senha_usuario)VALUES(?,?,?,?,?,?)';
-$stmt = $conexao->prepare($sql);
-$stmt->bind_param('ssisss', $nome_usuario, $email_usuario, $cpf_usuario, $nascimento_usuario, $genero_usuario, $senha_usuario_hash);
+$sql_verify_cpf = 'SELECT CPF_usuario FROM academia.usuarios WHERE CPF_usuario = ?';
+$stmt_cpf = $conexao->prepare($sql_verify_cpf);
+$stmt_cpf->bind_param('s', $cpf_usuario);
+$stmt_cpf->execute();
+$result_cpf = $stmt_cpf->get_result();
 
-if ($stmt->execute()) {
-    echo 'Usuário cadastrado com sucesso!';
+$sql_verify_email = 'SELECT email_usuario FROM academia.usuarios WHERE email_usuario = ?';
+$stmt_email = $conexao->prepare($sql_verify_email);
+$stmt_email->bind_param('s', $email_usuario);
+$stmt_email->execute();
+$result_email = $stmt_email->get_result();
+
+if ($result_cpf->num_rows > 0) {
+    echo 'CPF já cadastrado!<br>';
+} elseif ($result_email->num_rows > 0) {
+    echo 'Email já cadastrado!<br>';
 } else {
-    echo 'Erro ao cadastrar usuário:';
+    $sql = 'INSERT INTO usuarios (nome_usuario, email_usuario, CPF_usuario, data_nascimento_usuario, genero_usuario, senha_usuario)VALUES(?,?,?,?,?,?)';
+    $stmt = $conexao->prepare($sql);
+    $stmt->bind_param('ssisss', $nome_usuario, $email_usuario, $cpf_usuario, $nascimento_usuario, $genero_usuario, $senha_usuario_hash);
+
+    if ($stmt->execute()) {
+        echo 'Usuário cadastrado com sucesso!';
+    } else {
+        echo 'Erro ao cadastrar usuário:';
+    }
+    $stmt->close();
+    $conexao->close();
+    $stmt_cpf->close();
+    $stmt_email->close();
+
 }
-
-$stmt->close();
-$conexao->close();
-
-
 ?>
