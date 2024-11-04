72fd83eef6ea8fc913fb1332a0dff92c29ec781d
dogo-o <gabriel.luanh@yahoo.com.br>
2024-11-04 18:42:43 -0300

    aaaa

--- cadastro_usuario/script.js
+++ cadastro_usuario/script.js
@@ -1,5 +1,6 @@
 const botoes = document.querySelectorAll('#btn_usuario, #btn_nutri, #btn_treinador');
-const forms = document.querySelectorAll('#formulario_usuario, #formulario_nutricionista, #formulario_treinador');
+const cruds = document.querySelectorAll('#crud_usuario, #crud_nutri, #crud_treinador');
+const forms = document.querySelectorAll('#formulario_usuario, #formulario_nutricionista, #formulario_treinador')
 
 function aplicarEstiloAtivo(botao) {
     const idBotao = document.getElementById(botao);
@@ -9,12 +10,12 @@ function aplicarEstiloAtivo(botao) {
     idBotao.classList.add('ativo');
 }
 
-function mostrarFormulario(formulario) {
-    const idForm = document.getElementById(formulario);
-    for (form of forms) {
-        form.style.display = 'none';
+function mostrarCrud(crud) {
+    const idCrud = document.getElementById(crud);
+    for (x of cruds) {
+        x.style.display = 'none';
     }
-    idForm.style.display = 'block';
+    idCrud.style.display = 'block';
 }
 
 function mostrarSenha() {
@@ -28,19 +29,91 @@ function mostrarSenha() {
     }
 }
 
+function mostrarForm(form){
+    const idForm = document.getElementById(form)
+    const selectUser = document.getElementById('select_user')
+    for (x of forms) {
+        x.style.display = 'none';
+    }
+    for (y of cruds) {
+        y.style.display = 'none';
+    }
+    idForm.style.display = 'block'
+    selectUser.style.display = 'none'
+}
+
+function voltar(btnVoltar,respective_crud){
+    idBtn = document.getElementById(btnVoltar)
+    const selectUser = document.getElementById('select_user')
+    const crudUser = document.getElementById(respective_crud)
+    for (x of forms) {
+        x.style.display = 'none';
+    }
+    selectUser.style.display = 'flex'
+    crudUser.style.display = 'block'
+}
+
+
+//fetch - formulario
+
+document.getElementById('cadastro_usuario').addEventListener('submit', function(event) {
+    event.preventDefault();
+    console.log('Formulário enviado');
+    
+    const formData = new FormData(this);
+    
+    fetch('cadastrar_usuario.php', {  
+        method: 'POST',
+        body: formData
+    })
+    .then(response => {
+        console.log('Resposta recebida'); 
+        return response.text(); 
+    })
+    .then(data => {
+        document.getElementById('responseMessage').innerHTML = data;
+        console.log(data); 
+    })
+    .catch(error => {
+        console.error('Erro:', error); 
+    });
+});
+
+
+
+
+// botao select user
+
 document.getElementById('btn_usuario').addEventListener('click', function() {
     aplicarEstiloAtivo('btn_usuario');
-    mostrarFormulario('formulario_usuario');
+    mostrarCrud('crud_usuario');
 });
 document.getElementById('btn_nutri').addEventListener('click', function() {
     aplicarEstiloAtivo('btn_nutri');
-    mostrarFormulario('formulario_nutricionista');
+    mostrarCrud('crud_nutri');
 });
 document.getElementById('btn_treinador').addEventListener('click', function() {
     aplicarEstiloAtivo('btn_treinador');
-    mostrarFormulario('formulario_treinador');
+    mostrarCrud('crud_treinador');
 });
 
+// botao add
+
+document.getElementById('add_usuario').addEventListener('click', function(){
+    mostrarForm('formulario_usuario')})
+document.getElementById('add_nutri').addEventListener('click', function(){
+    mostrarForm('formulario_nutricionista')})
+document.getElementById('add_treinador').addEventListener('click', function(){
+    mostrarForm('formulario_treinador')})
+
+// botao voltar
+
+document.getElementById('btn_voltar_form_usuario').addEventListener('click', function(){
+    voltar('btn_voltar_form_usuario','crud_usuario')})
+document.getElementById('btn_voltar_form_nutri').addEventListener('click', function(){
+    voltar('btn_voltar_form_nutri','crud_nutri')})
+document.getElementById('btn_voltar_form_treinador').addEventListener('click', function(){
+    voltar('btn_voltar_form_treinador','crud_treinador')})
 
 
 
